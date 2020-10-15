1c75f46f6e90cba15e91773aa37ef27054ea164d
smassarsky <seth.massarsky@gmail.com>
2020-10-15 16:12:49 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.map{|appointment| appointment.patient}.uniq
+  end
+
+  def self.all
+    @@all
+  end
+
+end
